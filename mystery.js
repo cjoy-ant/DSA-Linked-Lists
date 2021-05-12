0de89e8e166b62d1eff697998a42e5689fc72787
Christine Antonio <christinejm.antonio@gmail.com>
2021-05-12 16:15:57 -0700

    initial commit

---
+++ mystery.js
@@ -0,0 +1,15 @@
+function WhatDoesThisProgramDo(lst) {
+  let current = lst.head;
+  while (current !== null) {
+      let newNode = current;
+      while (newNode.next !== null) {
+          if (newNode.next.value === current.value) {
+              newNode.next = newNode.next.next;
+          }
+          else {
+              newNode = newNode.next;
+          }
+      }
+      current = current.next;
+  }
+}
