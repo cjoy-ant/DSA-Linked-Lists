1b882ef4de77d76fabef93fe1ea52a26dbfb4123
Christine Antonio <christinejm.antonio@gmail.com>
2021-05-12 18:26:31 -0700

    supplemental functions

--- src/main.js
+++ src/main.js
@@ -20,7 +20,72 @@ const main = () => {
   SLL.insertBefore('Athena', 'Boomer')
   SLL.insertAfter('Hotdog', 'Helo')
   SLL.insertAt('Kat', 3)
-  SLL.remove('Tauhida')
+  SLL.remove('Tauhida')  
+
+  // display the linked list
+  const display = (LL) => {
+    let currNode = LL.head
+    while (currNode !== null) {
+      console.log(currNode)
+      currNode = currNode.next
+    }
+  }
+  
+  display(SLL)
+
+  // return the size of the linked list
+  const size = (LL) => {
+    let currNode = LL.head
+    let counter = 0
+    while (currNode !== null){
+      counter ++
+      currNode = currNode.next
+    }
+    console.log("Size: " + counter)
+    return counter
+  }
+
+  size(SLL)
+
+  // finds if the list is empty or not (without size size() function)
+  const isEmpty = (LL) => {
+    let isEmpty = true
+    if (LL.head) {
+      isEmpty = false
+    } 
+    console.log("the list is empty: " + isEmpty)
+    return isEmpty
+  }
+
+  isEmpty(SLL)
+
+  // finds the node before the item you are looking for
+  const findPrevious = (LL, item) => {
+    let currNode = LL.head
+    while (currNode !== null) {
+      if (currNode.next.value === item) {
+        console.log(`node before '${item}': ` + currNode.value)
+        return currNode.value
+      }
+      currNode = currNode.next
+    }
+  }
+
+  findPrevious(SLL, 'Helo')
+
+  // return the last node in the linked list
+  const findLast = (LL) => {
+    let currNode = LL.head
+    while (currNode !== null) {
+      if (currNode.next === null) {
+        console.log("last node: " + currNode.value)
+        return currNode.value
+      }
+      currNode = currNode.next
+    }
+  }
+
+  findLast(SLL)
 };
 
 module.exports = main;
