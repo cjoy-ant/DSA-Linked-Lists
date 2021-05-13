388e5565486e9899c6da5ba16ff28b96a30f30cb
Christine Antonio <christinejm.antonio@gmail.com>
2021-05-12 17:58:35 -0700

    insertBefore, insertAfter, insertAt

--- src/main.js
+++ src/main.js
@@ -1,16 +1,26 @@
-const LinkedList = require("./linkedList")
+const LinkedList = require("./linkedList");
 
 const main = () => {
-  let SLL = new LinkedList()
+  let SLL = new LinkedList();
 
-  SLL.insertFirst('Apollo')
-  SLL.insertLast('Boomer')
-  SLL.insertLast('Helo')
-  SLL.insertLast('Husker')
-  SLL.insertLast('Starbuck')
-  console.log(SLL)
-  SLL.insertLast('Tauhida')
-  SLL.remove('Husker')
-}
+  SLL.insertFirst("Apollo");
+  SLL.insertLast("Boomer");
+  SLL.insertLast("Helo");
+  SLL.insertLast("Husker");
+  SLL.insertLast("Starbuck");
+  console.log(SLL);
+  SLL.insertLast("Tauhida");
+  SLL.remove("Husker");
+  SLL.insertBefore("Test", "Helo");
+  SLL.remove("Test");
+  SLL.insertAfter("Test", "Apollo");
+  SLL.remove("Test");
+  SLL.insertAt('Test', 3)
+  SLL.remove('Test')
+  SLL.insertBefore('Athena', 'Boomer')
+  SLL.insertAfter('Hotdog', 'Helo')
+  SLL.insertAt('Kat', 3)
+  SLL.remove('Tauhida')
+};
 
-module.exports = main
+module.exports = main;
