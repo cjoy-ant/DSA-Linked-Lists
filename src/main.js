61a6b240f0cbc48bb100695d4bb099f8a392955c
Christine Antonio <christinejm.antonio@gmail.com>
2021-05-12 19:12:39 -0700

    reverse list iterative, thirdtolast

--- src/main.js
+++ src/main.js
@@ -86,6 +86,83 @@ const main = () => {
   }
 
   findLast(SLL)
+
+  /* REVERSE A LIST 
+  reverse a linked list with linear O(n) time complexity
+  revsere the direction of a given singly linekd list (all pointers should point backwards)
+  BONUS: solve using both recursive and iterative algorithms
+  */
+
+  const reverseListR = (LL) => {
+    // start at the head
+    let currNode = LL.head
+    // this saves the previous node/value to point at
+    let node = null
+    // loops through list
+    while (currNode !== null) { // [1, 2, 3]
+      let tempNext = currNode.next // 2
+      currNode.next = node // 1 -> null
+      node = currNode // node value now 1
+      currNode = tempNext // currNode now 2
+    }
+    LL.head = node // becomes the last item in the list
+    console.log(LL)
+  }
+
+  reverseListR(SLL)
+
+  const reverseListI = (LL) => {
+    // if list is empty
+    if (!LL.head) {
+      return null
+    }
+    // base case
+    // if 1 node in list 
+    if (LL.head.next === null) {
+      return LL.head
+    }
+    // general case
+  }
+  //unfinished
+  reverseListI(SLL)
+
+  // 3rd FROM THE END
+  const thirdToLast = (LL) => {
+    let currNode = LL.head
+    while (currNode !== null) {
+      if (currNode.next.next.next === null) {
+        console.log('third to last node: ' + currNode.value)
+        return currNode.value
+      }
+      currNode = currNode.next
+    }
+  }
+
+  thirdToLast(SLL)
+
+  // MIDDLE OF THE LIST
+  // const middleOfList = (LL) => {
+  //   let currNode = LL.head
+  //   let counter = size(LL)
+  //   let middle = Math.floor(counter/2)
+  //   for (let i=0; i < middle; i++) {
+  //     if (i === middle-1) {
+  //       console.log(currNode.value)
+  //       return currNode.value
+  //     }
+  //     currNode = currNode.next
+  //   }
+  // }
+
+  const middleOfList = (LL) => {
+    
+  }
+
+  middleOfList(SLL)
+
+  // CYCLCE IN A LIST
+
+  // SORTING A LIST
 };
 
 module.exports = main;
--- src/mystery.js
+++ src/mystery.js
@@ -1,11 +1,17 @@
 function WhatDoesThisProgramDo(lst) {
   let current = lst.head;
+  // loops through nodes
   while (current !== null) {
+    // decalres a newNode variable to keep track as it loops through
       let newNode = current;
+      // loops through nodes, excluding the last node
       while (newNode.next !== null) {
+        // if the newNode is equal to the head
           if (newNode.next.value === current.value) {
+            // it sets the next node as the next following node
               newNode.next = newNode.next.next;
           }
+          // otherwise, the newNode is set as the next node
           else {
               newNode = newNode.next;
           }
@@ -13,3 +19,5 @@ function WhatDoesThisProgramDo(lst) {
       current = current.next;
   }
 }
+
+// TIME COMPLEXITY: Polynomial O(n^2)
